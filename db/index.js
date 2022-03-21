88b51c45054acbff0cc41ed43c088e868bd2ccd4
Janae Edwards <edwards.janae@gmail.com>
2022-03-21 18:29:43 -0400

    toggle working plus some CSS

--- db/index.js
+++ db/index.js
@@ -21,7 +21,7 @@ const Album = sequelize.define('album', {
     },
     artistName: {
         type: Sequelize.STRING,
-        defaultValue: "Some Artist",
+        defaultValue: "An Amazing Artist",
         allowNull: false,
         validate: {notEmpty: true}
     },
@@ -44,7 +44,7 @@ const syncAndSeed = async () => {
     await Album.create({albumName: "Another Place and Time", monthReleased: "March 1989", genre: "Disco", artistName: "Donna Summer", listened: false});
     await Album.create({albumName: "Bulletproof Heart", monthReleased: "July 1989", genre: "New Wave", artistName: "Grace Jones", listened: false});
     await Album.create({albumName: "Crossroads", monthReleased: "October 1989", genre: "Blues Rock", artistName: "Tracey Chapman", listened: false});
-    await Album.create({albumName: "Hail the Queen", monthReleased: "November 1989", genre: "Hip-Hop", artistname: "Queen Latifah", listened: false})
+    await Album.create({albumName: "All Hail the Queen", monthReleased: "November 1989", genre: "Hip-Hop", artistName: "Queen Latifah", listened: false})
 }
 
 module.exports = {
--- src/AllAlbums.js
+++ src/AllAlbums.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { deleteAlbum } from './store';
+import { updateAlbum } from './store';
 
 // contains full list of albums, and delete button for each, and toggle for listened to albums
 
-const AllAlbums = ({ albums }) => {
+const AllAlbums = ({ albums, update }) => {
 
     return (
         <div>
             <ul>
                 {albums.map(album => {
                     return (
-                    <li key={album.id}>
-                        {album.albumName} <button onClick={ () => deleteAlbum(album) }> remove </button>
+                    <li onClick={ () => update(album)} key={album.id} className={ album.listened ? 'listened': '' }> 
+                        {album.albumName} by {album.artistName} <button onClick={ () => deleteAlbum(album) }> remove </button>
                     </li>
                     )
                 })}
@@ -23,8 +24,10 @@ const AllAlbums = ({ albums }) => {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-
+        update: (album) => {
+            dispatch(updateAlbum(album));
+        }
     }
 }
 
-export default connect(state => state)(AllAlbums)
+export default connect(state => state, mapDispatchToProps)(AllAlbums)
